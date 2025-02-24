e1adfd5e7d59266967a4a45352944b7b55eb82a9
Javi Abollado <javierabollado@gmail.com>
2025-02-24 10:24:32 -0500

    create run.sh for a long run

--- run.sh
+++ run.sh
@@ -2,11 +2,28 @@
 
 # List of model names
 models=("resnet50" "vgg16" "resnet152" "vgg19")
+lrs = (0.01 0.001)
+optims = ("adam" "radam" "sgd")
 
 # Loop through the models and run the Python script
+
+
+# testing different optimizers in vgg16
+for optim in "${optims[@]}" 
+do
+    for lr in "${lrs[@]}"
+    do
+        echo "Training model vgg16 [optim=$optim, lr=$lr]"
+        python main.py --name="vgg16_$optim_$lr" --gruop="vgg16" --model="vgg16" --epochs=10 --lr=$lr --optim=$optim
+    done
+done
+
+
+
+# testing different models
 for model in "${models[@]}"
 do
     echo "Training model: $model"
-    python main.py --name="$model" --gruop="dataagumentation" --model="$model" --epochs=10 --lr=0.001
+    python main.py --name="$model" --gruop="models" --model="$model" --epochs=30 --lr=0.001
 done
 
